55b5e6e47187a4fee1ce239fd3c8bbab085b5106
Eduarda Biajo Tavares <eduarda.bt.veleiros@gmail.com>
2025-05-14 16:02:36 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -74,6 +74,17 @@
         })
     }
 
+    window.addEventListener('load', () => {
+        let portfolioContainer = select(".portfolio-container")
+        if (portfolioContainer) {
+            let portfolioIsotope = new Isotope(portfolioContainer, {
+                itemsSelector: '.portfolio-item'
+            });
+
+            let portfolioFilters = select('#portfolio-filters li', true);
+        }
+    });
+
 
 
 })()
